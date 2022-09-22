60ab50afa6c9dd25aa2f69acd74831705b5abb17
vamsi1130 <113491695+vamsi1130@users.noreply.github.com>
2022-09-22 09:34:47 +0530

    Add files via upload

---
+++ sorting.R
@@ -0,0 +1,12 @@
+> x = c(10, 20, 30, 25, 9, 26)
+> print("Original Vectors:")
+[1] "Original Vectors:"
+> print(x)
+[1] 10 20 30 25  9 26
+> print("Sort in ascending order:")
+[1] "Sort in ascending order:"
+> print(sort(x))
+[1]  9 10 20 25 26 30
+> print("Sort in descending order:")
+[1] "Sort in descending order:"
+> print(sort(x, decreasing=TRUE))
